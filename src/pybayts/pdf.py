4856baddfd3bb46631fea4d462ea5d9f7738082f
Ryan Avery <ravery@ucsb.edu>
2021-03-10 10:44:28 -0800

    fix indent

--- src/pybayts/pdf.py
+++ src/pybayts/pdf.py
@@ -13,16 +13,19 @@ from typing import Tuple
 import numpy as np
 import scipy.stats as stats
 
-def calc_pnf(time_series: np.ndarray, pdf: Tuple, bwf: Tuple = (0, 1)) -> np.ndarray:
+
+def calc_pnf(
+    time_series: np.ndarray, pdf: Tuple, bwf: Tuple = (0, 1)
+) -> np.ndarray:
     """Calculating conditional non-forest probability (PNF).
 
     Args:
         time_series (np.ndarray): np.ndarray containing time series at a single pixel.
-        pdf (Tuple): Tuple describing forest and nonforest distributions. 
-            pdf[0] = pdf type for Forest, pdf[1] = pdf type for Nonforest, pdf[2] and pdf[3] = pdf 
-            parameters describing Forest, pdf[5] and pdf[6] = pdf parameter describing Nonforest. 
+        pdf (Tuple): Tuple describing forest and nonforest distributions.
+            pdf[0] = pdf type for Forest, pdf[1] = pdf type for Nonforest, pdf[2] and pdf[3] = pdf
+            parameters describing Forest, pdf[5] and pdf[6] = pdf parameter describing Nonforest.
             pdf types supported: Gaussian or Weibull.
-        bwf (Tuple, optional): Block weighting function to truncate the NF probability. 
+        bwf (Tuple, optional): Block weighting function to truncate the NF probability.
             Defaults to (0,1) = no truncation.
 
     Returns:
@@ -38,30 +41,27 @@ def calc_pnf(time_series: np.ndarray, pdf: Tuple, bwf: Tuple = (0, 1)) -> np.nda
         calc_pnf(ndvi, pdf)
     """
 
-  if len(time_series) > 0:
-    #Gaussian pdf (mean and sd)
-    if pdf[0] == "gaussian":
-        pf = stats.norm.pdf(x=time_series, loc=pdf[2], scale=pdf[3])
-    elif pdf[0] == "weibull":
-        pf = stats.weibull.pdf(x=time_series, shape=pdf[2], scale=pdf[3])
-    else:
-        raise ValueError("Must supply 'gaussian' or 'weibull' for pdf[0].")
+    if len(time_series) > 0:
+        # Gaussian pdf (mean and sd)
+        if pdf[0] == "gaussian":
+            pf = stats.norm.pdf(x=time_series, loc=pdf[2], scale=pdf[3])
+        elif pdf[0] == "weibull":
+            pf = stats.weibull.pdf(x=time_series, shape=pdf[2], scale=pdf[3])
+        else:
+            raise ValueError("Must supply 'gaussian' or 'weibull' for pdf[0].")
 
-    #Weibull pdf (shape and scale), TODO figure out why loc (the mean) wasn't supplied for this distribution
-    if pdf[1] == "gaussian":
-        pnf = stats.norm.pdf(x=time_series, loc=pdf[4], scale=pdf[5])
-    elif pdf[1] == "weibull":
-        pnf = stats.weibull.pdf(x=time_series, scale=pdf[5], scale=pdf[5])
-    else:
-        raise ValueError("Must supply 'gaussian' or 'weibull' for pdf[1].")
+        # Weibull pdf (shape and scale), TODO figure out why loc (the mean) wasn't supplied for this distribution
+        if pdf[1] == "gaussian":
+            pnf = stats.norm.pdf(x=time_series, loc=pdf[4], scale=pdf[5])
+        elif pdf[1] == "weibull":
+            pnf = stats.weibull.pdf(x=time_series, scale=pdf[5], scale=pdf[5])
+        else:
+            raise ValueError("Must supply 'gaussian' or 'weibull' for pdf[1].")
 
-    #calculate conditinal NF
-    pnf[pnf < 1e-10000] = 0
-    pnf[pnf > 0] = (pnf[pnf > 0] / (pf [pnf > 0] + pnf[pnf > 0]))
-    ## apply block weighting function
-    pnf[pnf < bwf[0]] = bwf[0]
-    pnf[pnf > bwf[1]] = bwf[1]
-  return(pnf)
-
-
-
+        # calculate conditinal NF
+        pnf[pnf < 1e-10000] = 0
+        pnf[pnf > 0] = pnf[pnf > 0] / (pf[pnf > 0] + pnf[pnf > 0])
+        ## apply block weighting function
+        pnf[pnf < bwf[0]] = bwf[0]
+        pnf[pnf > bwf[1]] = bwf[1]
+        return pnf
