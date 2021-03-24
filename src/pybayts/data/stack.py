fix ndvi masking, debug stacking funcs, add qa module with usgs tiler code for parsing bitpacked qa flags, add reqs

src/pybayts/data/stack.py
@@ -6,6 +6,9 @@ import xarray as xr
 from iteration_utilities import groupedby
 import numpy as np
 import rasterio
+import json
+import geopandas as gpd
+from geocube.api.core import make_geocube
 
 
 def get_scene_paths(csv_path: str):
@@ -20,70 +23,38 @@ def get_scene_paths(csv_path: str):
     """
     scenes = pd.read_csv(csv_path)
     if "GRD" in csv_path:
-        scenes["Date"] = scenes["Granule Name"].apply(lambda x: x[17:25])
+        scenes = scenes["Granule Name"].tolist()
     elif "ls8" in csv_path:
-        scenes["Date"] = scenes["azure_path"].apply(lambda x: x[111:119])
-    # dates_lst = scenes.Date.tolist()
+        scenes = scenes["azure_path"].tolist()
     return scenes
 
 
-def scene_id_to_ndvi_arr(root_dir: str, landsat_scene_id: str) -> str:
+def scene_id_to_ndvi_arr(outdir: str, b4_path: str, b5_path: str) -> str:
     """Function to save NDVI for a Landsat 8 scene.
 
     NDVI = (Red - NIR) / (Red + NIR)
 
     Args:
-        root_dir (str): The path to the dir containing landsat scene folders.
+        outdir (str): The path to the dir to save landsat scene folders with ndvi.
         landsat_scene_id (str): The Landsat scene id / folder name
 
     Returns:
         str: The path to the NDVI .tif
     """
+    # confirm scene ids are the same
+    assert "B4" in b4_path
+    assert "B5" in b5_path
+    scene_id_b4 = b4_path.split("/")[-2]
+    assert scene_id_b4 == b5_path.split("/")[-2]
+    red = rio.open_rasterio(b4_path, masked=True)  # red
+    nir = rio.open_rasterio(b5_path, masked=True)  # near infrared
 
-    landsat_dir = os.path.join(root_dir, landsat_scene_id)
+    calc_ndvi = (nir - red) / (nir + red)
+    ndvi_path = os.path.join(outdir, scene_id_b4 + "_NDVI.tif")
 
-    if not os.path.exists(landsat_dir):
-        os.makedirs(landsat_dir)
-
-    src_4 = rasterio.open(
-        os.path.join(root_dir, landsat_scene_id + "_B4.TIF")
-    )  # red
-    src_5 = rasterio.open(
-        os.path.join(root_dir, landsat_scene_id + "_B5.TIF")
-    )  # near infrared
-
-    meta = src_5.meta
-
-    # arrays
-    red = src_4.read()
-    nir = src_5.read()
-
-    calc_ndvi = np.where((nir + red) == 0.0, 0, (nir - red) / (nir + red))
-    ndvi_path = os.path.join(landsat_dir, landsat_scene_id + "_NDVI.tif")
-
-    with rasterio.open(ndvi_path, "w", **meta) as dst:
-        dst.write(calc_ndvi.astype(rasterio.uint16))
+    calc_ndvi.to_raster(ndvi_path)
 
     return ndvi_path
-
-
-def open_merge_per_date(paths: List[str]):
-    """Opens each path in a list of paths and merges with rioxarray.
-
-    Args:
-        paths (List[str]): List of paths for the same date.
-
-    Returns:
-        xarray.DataArray: A DataArray with geospatial metadata in .rio.
-    """
-    arrs = []
-    for p in paths:
-        arr = rio.open_rasterio(p)
-        arrs.append(arr)
-    name = arr.name
-    merged = rio.merge.merge_arrays(arrs)
-    merged.name = "merged_" + name
-    return merged
 
 
 def groupby_date(scenes: List[str]) -> List[List]:
@@ -105,9 +76,42 @@ def groupby_date(scenes: List[str]) -> List[List]:
     return group_dict
 
 
-def merge_each_date(date_group_dict, outfolder):
+def open_geojson(geojson_path):
+    with open(geojson_path) as f:
+        feature = json.load(f)
+    return feature
+
+
+def make_aoi_grid(geojson_path: str, epsg: str = "EPSG:32722"):
+    aoi_gdf = gpd.read_file(geojson_path)
+    # utm epsg for brazil since landsat and sentinel in utm
+    aoi_grid_ds = make_geocube(aoi_gdf, output_crs=epsg, resolution=(30, 30))
+    return aoi_grid_ds
+
+
+def open_merge_per_date(paths: List[str]):
+    """Opens each path in a list of paths and merges with rioxarray.
+
+    Args:
+        paths (List[str]): List of paths for the same date.
+
+    Returns:
+        xarray.DataArray: A DataArray with geospatial metadata in .rio.
+    """
+    arrs = []
+    for p in paths:
+        arr = rio.open_rasterio(p)
+        arrs.append(arr)
+    name = arr.name
+    merged = rio.merge.merge_arrays(arrs)
+    merged.name = "merged_" + name
+    return merged
+
+
+def merge_each_date(date_group_dict, aoi_grid, outfolder):
     """Takes a group dict from groupby_date and iterates through to merge and save rasters.
 
+    Also aligns the output array to the aoi grid so the results can be stacked into a time series.
     Args:
         date_group_dict (dict): A dictionary with keys as str dates and values a tuple of paths.
         outfolder (str): Where to save the merged rasters. Full path to the folder.
@@ -122,6 +126,25 @@ def merge_each_date(date_group_dict, outfolder):
         else:
             merged_date_arr = rio.open_rasterio(group[0])
             merged_date_arr.name = group[0]
-    outpath = os.path.join(outfolder, merged_date_arr.name)
-    merged_date_arr.to_raster(outpath)
-    return outpath
+        outpath = os.path.join(outfolder, merged_date_arr.name)
+        merged_date_arr = reproject_match_to_aoi(merged_date_arr, aoi_grid)
+        merged_date_arr.to_raster(outpath)
+        return outpath
+
+
+def reproject_match_to_aoi(da, match_da):
+    """Reprojects Sentinel-1 or Landsat to 30,30 meter resolution and snaps to a common grid.
+
+    Also makes all arrays same extent, with NaNs for no observations.
+
+    Args:
+        da (xr.DataArray): The xr.DataArray for a single date.
+        match_da (xr.Dataset): The aoi grid. Can be created with
+            make_geocube(aoi_gdf, output_crs="EPSG:32722", resolution=(30,30)).
+            Make sure to use the correct EPSG for the AOI.
+
+    Returns:
+        xr.DataArray: The processed xr.DataArray, ready to be stacked into a time series.
+    """
+
+    return da.rio.reproject_match(match_da)