920a145c60813e14ade14fcac2547157b278f72a
Lilly Thomas <lillythomas@Lillys-MacBook-Pro.local>
2021-04-28 14:43:12 -0500

    modifications to reduce redundancy in favor of utility functions

--- src/pybayts/stats.py
+++ src/pybayts/stats.py
@@ -33,83 +33,53 @@ def compute_stats(merged_dir_sar, merged_dir_ndvi, sar_subset_forest, sar_subset
     Returns:
         Average forest and non-forest mean and standard deviations from the SAR and NDVI time series.
     """
-    sar_stats_forest = [] 
-    sar_stats_nonforest = []
-    for sar in merged_dir_sar:
-        sarr = rio.open_rasterio(sar)
-        sarr.rio.reproject("EPSG:4326")
-        sarr.rio.to_raster(os.path.join(merged_dir_sar, sar[:-4]+"_4326.tif"))
-        forest_distr_sar = zonal_stats(sar_subset_forest, os.path.join(merged_dir_sar, sar[:-4]+"_4326.tif"),
-            stats="min mean max std count")
-        sar_stats_forest.append(forest_distr_sar)
-        nonforest_distr_sar = zonal_stats(sar_subset_nonforest, os.path.join(merged_dir_sar, sar[:-4]+"_4326.tif"),
-            stats="min mean max std count")   
-        sar_stats_nonforest.append(nonforest_distr_sar)
-        
-
-    ndvi_stats_forest = [] 
-    ndvi_stats_nonforest = []
-    for ndvi in merged_dir_ndvi:
-        ndvir = rio.open_rasterio(ndvi)
-        ndvir.rio.reproject("EPSG:4326")
-        ndvir.rio.to_raster(os.path.join(merged_dir_ndvi, ndvi[:-4]+"_4326.tif"))
-        forest_distr_ndvi = zonal_stats(ndvi_subset_forest, os.path.join(merged_dir_ndvi, ndvi[:-4]+"_4326.tif"),
-            stats="min mean max std count")
-        ndvi_stats_forest.append(forest_distr_ndvi)
-        nonforest_distr_ndvi = zonal_stats(ndvi_subset_nonforest, os.path.join(merged_dir_ndvi, ndvi[:-4]+"_4326.tif"),
-            stats="min mean max std count")   
-        ndvi_stats_nonforest.append(nonforest_distr_ndvi)
     
-    sar_stats_forest_mean_list = []
-    sar_stats_forest_std_list = []
-    for stats_ in sar_stats_forest:
-        stats_dict = stats_[0]
-        print(stats_dict)
-        stats_list = []
-        for key,value in stats_dict.items() :
-            stats_list.append(value)
-        mean, std = stats_list[2], stats_list[3]
-        sar_stats_forest_mean_list.append(mean)
-        sar_stats_forest_std_list.append(std)
-    sar_stats_forest = [statistics.mean(sar_stats_forest_mean), statistics.mean(sar_stats_forest_std)]
+    sar_stats_forest = [] 
+    sar_stats_nonforest = []    
+    ndvi_stats_forest = [] 
+    ndvi_stats_nonforest = []    
+    
+    def get_stats_dict(dir, geojson):
+        dist_list = []
+        for f in dir:
+            arr = rio.open_rasterio(f)
+            arr.rio.reproject("EPSG:4326")
+            arr.rio.to_raster(os.path.join(dir, f[:-4]+"_4326.tif"))
+            dist = zonal_stats(geojson, os.path.join(dir, f[:-4]+"_4326.tif"),
+            stats="min mean max std count")
+            dist_list.append(dist)
+            return dist_list
         
-    sar_stats_nonforest_mean_list = []
-    sar_stats_nonforest_std_list = []
-    for stats_ in sar_stats_nonforest:
-        stats_dict = stats_[0]
-        print(stats_dict)
-        stats_list = []
-        for key,value in stats_dict.items() :
-            stats_list.append(value)
-        mean, std = stats_list[2], stats_list[3]
-        sar_stats_nonforest_mean_list.append(mean)
-        sar_stats_nonforest_std_list.append(std)
-    sar_stats_nonforest = [statistics.mean(sar_stats_nonforest_mean), statistics.mean(sar_stats_nonforest_std)]
+    sar_dist_forest = get_stats_dict(merged_dir_sar, sar_subset_forest)
+    sar_dist_nonforest = get_stats_dict(merged_dir_sar, sar_subset_nonforest)
+    ndvi_stats_forest = get_stats_dict(merged_dir_ndvi, ndvi_subset_forest)
+    ndvi_stats_nonforest = get_stats_dict(merged_dir_ndvi, ndvi_subset_nonforest)
 
-    ndvi_stats_forest_mean_list = []
-    ndvi_stats_forest_std_list = []
-    for stats_ in ndvi_stats_forest:
-        stats_dict = stats_[0]
-        print(stats_dict)
-        stats_list = []
-        for key,value in stats_dict.items() :
-            stats_list.append(value)
-        mean, std = stats_list[2], stats_list[3]
-        ndvi_stats_forest_mean_list.append(mean)
-        ndvi_stats_forest_std_list.append(std)
-    ndvi_stats_forest = [statistics.mean(ndvi_stats_forest_mean), statistics.mean(ndvi_stats_forest_std)]
-        
-    ndvi_stats_nonforest_mean_list = []
-    ndvi_stats_nonforest_std_list = []
-    for stats_ in ndvi_stats_nonforest:
-        stats_dict = stats_[0]
-        print(stats_dict)
-        stats_list = []
-        for key,value in stats_dict.items() :
-            stats_list.append(value)
-        mean, std = stats_list[2], stats_list[3]
-        ndvi_stats_nonforest_mean_list.append(mean)
-        ndvi_stats_nonforest_std_list.append(std)
-    ndvi_stats_nonforest = [statistics.mean(ndvi_stats_nonforest_mean), statistics.mean(ndvi_stats_nonforest_std)]
+    
+    def get_averages(dist_list):
+        mean_list = []
+        std_list = []
+        for stats_ in dist_list:
+            stats_dict = stats_[0]
+            print(stats_dict)
+            stats_list = []
+            for key,value in stats_dict.items() :
+                stats_list.append(value)
+            mean, std = stats_list[2], stats_list[3]
+            mean_list.append(mean)
+            std_list.append(std)
+        return mean_list, std_list
+
+    sar_stats_forest_mean_list, sar_stats_forest_std_list = get_averages(sar_dist_forest)
+    sar_stats_forest = [statistics.mean(sar_stats_forest_mean_list), statistics.mean(sar_stats_forest_std_list)]
+    
+    sar_stats_nonforest_mean_list, sar_stats_nonforest_std_list = get_averages(sar_dist_nonforest)
+    sar_stats_nonforest = [statistics.mean(sar_stats_nonforest_mean_list), statistics.mean(sar_stats_nonforest_std_list)]
+    
+    ndvi_stats_forest_mean_list, ndvi_stats_forest_std_list = get_averages(ndvi_stats_forest)
+    ndvi_stats_forest = [statistics.mean(ndvi_stats_forest_mean_list), statistics.mean(ndvi_stats_forest_std_list)]
+    
+    ndvi_stats_nonforest_mean_list, ndvi_stats_nonforest_std_list = get_averages(ndvi_stats_nonforest)
+    ndvi_stats_forest = [statistics.mean(ndvi_stats_nonforest_mean_list), statistics.mean(ndvi_stats_nonforest_std_list)]
 
     return sar_stats_forest, sar_stats_nonforest, ndvi_stats_forest, ndvi_stats_nonforest
